395432e69e5a3cba9e494110e7d2b1bcd9f1f9cb
Pushkar Gupte <pushkar@bravenewcoin.com>
2018-10-12 15:56:03 +1300

    Use promises to get the OAuth2 token
    Signed-off-by: Pushkar Gupte <pushkar@bravenewcoin.com>

--- app.js
+++ app.js
@@ -1,38 +1,30 @@
-const ws = require('ws');
-const request = require('request');
-
+const websocket = require('ws');
 const client = require('./client.js');
-const oauth2 = require('./oauth2');
 
-request.post("https://auth.bravenewcoin.com/oauth/token", {
-  json: true,
-  body: oauth2
-}, (err, res, body) => {
-  if (err) {
-    return console.log(err);
-  }
-  const ws = new ws('wss://ws.bravenewcoin.com?access_token=' + body.access_token, {});
+client.getBearerToken().then(response => {
+      const ws = new websocket('wss://ws.bravenewcoin.com?access_token=' + response.access_token, {});
 
-  ws.on('open', async function open() {
-    console.log('connected');
-    ws.send("{'event':'PING'}");
+      ws.on('open', async function open() {
+        console.log('connected');
+        ws.send("{'event':'PING'}");
 
-    client.getMarketId('BTC', 'KRW')
-    .then(market => ws.send("{'event':'SUBSCRIBE_TICKER','marketId':" + market.content[0].id + "}"));
+        client.getMarketId('BTC', 'KRW')
+        .then(market => ws.send("{'event':'SUBSCRIBE_TICKER','marketId':" + market.content[0].id + "}"));
 
-    client.getMarketId('BTC', 'EUR')
-    .then(market => ws.send("{'event':'SUBSCRIBE_TICKER','marketId':" + market.content[0].id + "}"));
+        client.getMarketId('BTC', 'EUR')
+        .then(market => ws.send("{'event':'SUBSCRIBE_TICKER','marketId':" + market.content[0].id + "}"));
 
-    ws.on('close', function close() {
-      console.log('disconnected');
-    });
+        ws.on('close', function close() {
+          console.log('disconnected');
+        });
 
-    ws.on('error', function error(e) {
-      console.error('error occured', e);
-    });
+        ws.on('error', function error(e) {
+          console.error('error occured', e);
+        });
 
-    ws.on('message', function incoming(data) {
-      console.log(data);
-    });
-  });
-});
+        ws.on('message', function incoming(data) {
+          console.log(data);
+        });
+      });
+    }
+);
--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const request = require('request-promise-native');
+const oauth2 = require('./oauth2');
 
 async function getAssetId(symbol) {
   const asset = await request.get("https://api.bravenewcoin.com/v3/asset?symbol=" + symbol, {json: true});
@@ -11,5 +12,13 @@ async function getMarketId(baseSymbol, quoteSymbol) {
   return request.get("https://api.bravenewcoin.com/v3/market?baseAssetId=" + baseAssetId + "&quoteAssetId=" + quoteAssetId, {json: true});
 }
 
+async function getBearerToken() {
+  return request.post("https://auth.bravenewcoin.com/oauth/token", {
+    json: true,
+    body: oauth2
+  })
+}
+
 module.exports.getAssetId = getAssetId;
 module.exports.getMarketId = getMarketId;
+module.exports.getBearerToken = getBearerToken;
