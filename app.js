6b9d7bec565d144318aa3b7e0569b48c3df01600
james <james.little@bravenewcoin.com>
2020-07-02 14:28:56 +1200

    Updated examples with LX, Trades and GWA / MWA Indices

--- app.js
+++ app.js
@@ -1,30 +1,88 @@
-const websocket = require('ws');
+const WebSocket = require('ws');
 const client = require('./client.js');
 
+let requestId = 1;
+
+
 client.getBearerToken().then(response => {
-    const ws = new websocket('wss://ws.bravenewcoin.com?access_token=' + response.access_token, {});
+    const ws = new WebSocket('wss://ws.bravenewcoin.com?access_token=' + response.access_token, undefined, {});
 
-    ws.on('open', async function open() {
+    ws.onclose = function close(event) {
+        console.log('disconnected');
+        if (event.wasClean) {
+            console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+        } else {
+            // e.g. server process killed or network down event.code is usually 1006 in this case.
+            console.log('[close] Connection died');
+        }
+    };
+
+    ws.onerror = function error(error) {
+        console.error(`[error] ${error.message}`);
+    };
+
+    ws.onmessage = function incoming(event) {
+        console.log(`[message] Data received from server: ${event.data}`);
+    };
+
+    ws.onopen = function open() {
         console.log('connected');
-        ws.send('{\'event\':\'PING\'}');
 
+        // Send a PING every 60 seconds or so...
+        const interval = setInterval(() => ws.send(JSON.stringify({
+            event: 'PING',
+            requestId: requestId++
+        })), 60000);
+        ws.on('close', () => clearInterval(interval));
+
+        // Subscribe to all exchange tickers for Bitcoin to Korean Wan.
         client.getMarketId('BTC', 'KRW')
-            .then(market => ws.send('{\'event\':\'SUBSCRIBE_EXCHANGE_TICKER\',\'marketId\':' + market.content[0].id + '}'));
+            .then(market => ws.send(JSON.stringify({
+                event: 'SUBSCRIBE_EXCHANGE_TICKER',
+                marketId: market,
+                requestId: requestId++
+            })));
 
-        client.getMarketId('BTC', 'EUR')
-            .then(market => ws.send('{\'event\':\'SUBSCRIBE_EXCHANGE_TICKER\',\'marketId\':' + market.content[0].id + '}'));
+        // Subscribe to BTC/USDT on Binance exchange.
+        Promise.all([
+            client.getMarketId('BTC', 'USDT'),
+            client.getExchangeId('binance')
+        ]).then((results) => ws.send(JSON.stringify({
+            event: 'SUBSCRIBE_EXCHANGE_TICKER',
+            marketId: results[0],
+            exchangeId: results[1],
+            requestId: requestId++
+        }))).catch(error => console.error('Subscribe to binance BTC/USDT failed', error));
 
-        ws.on('close', function close() {
-            console.log('disconnected');
-        });
+        // Subscribe to trades on BTSE.
+        client.getExchangeId('btse')
+            .then(exchange => ws.send(JSON.stringify({
+                event: 'SUBSCRIBE_TRADE',
+                exchangeId: exchange,
+                requestId: requestId++
+            })));
 
-        ws.on('error', function error(e) {
-            console.error('error occured', e);
-        });
+        // Subscribe to the BLX.
+        ws.send(JSON.stringify({
+            event: 'SUBSCRIBE_INDEX_TICKER',
+            indexType: 'LX',
+            requestId: requestId++
+        }));
 
-        ws.on('message', function incoming(data) {
-            console.log(data);
-        });
-    });
-}
-);
+        // Subscribe to Bitcoin GWA index.
+        client.getAssetId('BTC')
+            .then(asset => ws.send(JSON.stringify({
+                event: 'SUBSCRIBE_INDEX_TICKER',
+                indexId: asset,
+                requestId: requestId++
+            })));
+
+        // Subscribe to the Bitcoin/Korean won MWA index.
+        client.getMarketId('BTC', 'KRW')
+            .then(market => ws.send(JSON.stringify({
+                event: 'SUBSCRIBE_INDEX_TICKER',
+                indexId: market,
+                requestId: requestId++
+            })));
+    };
+});
--- client.js
+++ client.js
@@ -1,15 +1,33 @@
 const request = require('request-promise-native');
 const oauth2 = require('./oauth2');
 
+// Find the first asset ID matching the given symbol.
 async function getAssetId(symbol) {
     const asset = await request.get('https://api.bravenewcoin.com/v3/asset?symbol=' + symbol, {json: true});
+    if (asset.content.length > 1) {
+        const choices = asset.content.map( c => c.name).join('\',\'');
+        throw new Error(`Found multiple matches for '${symbol}': ['${choices}']`);
+    }
     return asset.content[0].id;
 }
 
+// Find the first market ID match for the given base and quote symbols.
 async function getMarketId(baseSymbol, quoteSymbol) {
     const baseAssetId = await getAssetId(baseSymbol);
     const quoteAssetId = await getAssetId(quoteSymbol);
-    return request.get('https://api.bravenewcoin.com/v3/market?baseAssetId=' + baseAssetId + '&quoteAssetId=' + quoteAssetId, {json: true});
+    const market = await request.get('https://api.bravenewcoin.com/v3/market?baseAssetId=' + baseAssetId + '&quoteAssetId=' + quoteAssetId, {json: true});
+    return market.content[0].id;
+}
+
+// Find the first exchange with the given exchange name.
+async function getExchangeId(name) {
+    const exchanges = await request.get('https://api.bravenewcoin.com/v3/exchange', {json: true});
+    for (let exchange of exchanges.content) {
+        if (name.localeCompare(exchange.name, undefined, { sensitivity: 'accent' }) === 0) {
+            return exchange.id;
+        }
+    }
+    throw new Error(`Unable to find exchange with name '${name}'`);
 }
 
 async function getBearerToken() {
@@ -21,4 +39,5 @@ async function getBearerToken() {
 
 module.exports.getAssetId = getAssetId;
 module.exports.getMarketId = getMarketId;
+module.exports.getExchangeId = getExchangeId;
 module.exports.getBearerToken = getBearerToken;
