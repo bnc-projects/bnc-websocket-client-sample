06ef670180ce0a22799d8969b80625dcde5ca4ea
james <james.little@bravenewcoin.com>
2020-09-16 11:02:28 +1200

    Updated for high frequency indices example using development env

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const WebSocket = require('ws');
 const client = require('./client.js');
 
 let requestId = 1;
-
+const indices = ['cc7887e1-1c67-4de5-bfd0-28a999b0e62a', '3692a276-66ec-4db6-a8d8-77bd2f10c7d0'];
 
 client.getBearerToken().then(response => {
     console.log(`token valid for ${response.expires_in} seconds`);
 
-    const ws = new WebSocket('wss://ws.bravenewcoin.com?access_token=' + response.access_token, undefined, {});
+    const ws = new WebSocket('wss://dev-ws.bravenewcoin.com?access_token=' + response.access_token, undefined, {});
 
     ws.onclose = function close(event) {
-        console.log('disconnected');
+        console.log('[disconnected]');
         if (event.wasClean) {
             console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
         } else {
@@ -24,67 +24,25 @@ client.getBearerToken().then(response => {
     };
 
     ws.onmessage = function incoming(event) {
-        console.log(`[message] Data received from server: ${event.data}`);
+        console.log(`[message] ${event.data}`);
     };
 
     ws.onopen = function open() {
-        console.log('connected');
+        console.log('[connected]');
 
         // Send a PING every 60 seconds or so...
         const interval = setInterval(() => ws.send(JSON.stringify({
             event: 'PING',
             requestId: requestId++
         })), 60000);
+
         ws.on('close', () => clearInterval(interval));
 
-        // Subscribe to all exchange tickers for Bitcoin to Korean Wan.
-        client.getMarketId('BTC', 'KRW')
-            .then(market => ws.send(JSON.stringify({
-                event: 'SUBSCRIBE_EXCHANGE_TICKER',
-                marketId: market,
-                requestId: requestId++
-            })));
-
-        // Subscribe to BTC/USDT on Binance exchange.
-        Promise.all([
-            client.getMarketId('BTC', 'USDT'),
-            client.getExchangeId('binance')
-        ]).then((results) => ws.send(JSON.stringify({
-            event: 'SUBSCRIBE_EXCHANGE_TICKER',
-            marketId: results[0],
-            exchangeId: results[1],
-            requestId: requestId++
-        }))).catch(error => console.error('Subscribe to binance BTC/USDT failed', error));
-
-        // Subscribe to trades on BTSE.
-        client.getExchangeId('btse')
-            .then(exchange => ws.send(JSON.stringify({
-                event: 'SUBSCRIBE_TRADE',
-                exchangeId: exchange,
-                requestId: requestId++
-            })));
-
-        // Subscribe to the BLX.
-        ws.send(JSON.stringify({
+        // Subscribe to the required indices.
+        indices.forEach(index => ws.send(JSON.stringify({
             event: 'SUBSCRIBE_INDEX_TICKER',
-            indexType: 'LX',
+            indexId: index,
             requestId: requestId++
-        }));
-
-        // Subscribe to Bitcoin GWA index.
-        client.getAssetId('BTC')
-            .then(asset => ws.send(JSON.stringify({
-                event: 'SUBSCRIBE_INDEX_TICKER',
-                indexId: asset,
-                requestId: requestId++
-            })));
-
-        // Subscribe to the Bitcoin/Korean won MWA index.
-        client.getMarketId('BTC', 'KRW')
-            .then(market => ws.send(JSON.stringify({
-                event: 'SUBSCRIBE_INDEX_TICKER',
-                indexId: market,
-                requestId: requestId++
-            })));
+        })));
     };
 });
